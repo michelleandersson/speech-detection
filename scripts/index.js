d4783d64690051df61cdc8bf3e9db5bd576158d4
michelleandersson <michelleandersson@live.com>
2020-06-18 18:43:06 +0200

    add js for working speech detection app

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+const btnStart = document.querySelector('.start')
+const btnEnd = document.querySelector('.end')
+
+recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+
+    const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+    p.textContent = poopScript;
+
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+});
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
+
+const handleStop = () => {
+    recognition.stop();
+    console.log('Speech recognition has stopped.');
+}
+
+btnEnd.addEventListener('click', handleStop)
